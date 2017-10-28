796a8c780b3aa28e6013f5efbaf019a3643df56b
pww24 <pawel.wach4@gmail.com>
2017-10-28 15:59:41 +0200

    początek

--- WindowsFormsApplication1/Program.cs
+++ WindowsFormsApplication1/Program.cs
@@ -17,6 +17,9 @@ namespace WindowsFormsApplication1
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+
+
         }
     }
 }
